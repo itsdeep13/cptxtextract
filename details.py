ba23e5364347ceb4102cc3e3c5d1f4f1131ff914
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2024-02-03 11:08:37 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
+api_id = "3498045"
+api_hash = "229441a8b355de58ba025be657c82ea3"
+bot_token = "6685547719:AAHRce4N90dhZu-Kx2cJm_KO_wvV_Sdtzco"
+auth_users = [6478217960]
+sudo_user = 6478217960
 log_channel = -1002143629860
 txt_channel = -1002049391187
